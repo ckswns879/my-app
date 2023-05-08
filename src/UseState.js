0079620066a875f37a49bdfcec8f4c51660b2771
chan jun <ckswns879@naver.com>
2023-05-08 17:00:29 +0900

    chore: 컴포넌트 주석처리

---
+++ src/UseState.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+
+
+function UseState() {
+//let number = 1; // 이렇게 하면 상태값 아님
+const [number, setNumber] = useState(1);  //React안에 hooks라이브러리
+
+const add = () => {
+setNumber(number+1); // 리엑트한테 number 값 변경한다고 요청 number++는안됨
+console.log('add',number);
+};
+// 랜더링 시점 = 상태값이 변경될때
+return (
+<>
+<div>
+<h1>숫자 : {number}</h1>
+<button onClick={add}>더하기</button>
+</div>
+</>
+);
+};
+
+export default UseState;
