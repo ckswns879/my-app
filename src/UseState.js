95aa505c8a7d310d405045a2c6af5ef9e56dd464
chan jun <ckswns879@naver.com>
2023-05-08 17:02:46 +0900

    feat: useState

--- src/UseState.js
+++ src/UseState.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 function UseState() {
 //let number = 1; // 이렇게 하면 상태값 아님
-const [number, setNumber] = useState(1);  //React안에 hooks라이브러리
+const [number, setNumber] = useState(1);  //상태값 //React안에 hooks라이브러리
 
 const add = () => {
 setNumber(number+1); // 리엑트한테 number 값 변경한다고 요청 number++는안됨
@@ -13,10 +13,10 @@ console.log('add',number);
 // 랜더링 시점 = 상태값이 변경될때
 return (
 <>
-<div>
-<h1>숫자 : {number}</h1>
-<button onClick={add}>더하기</button>
-</div>
+    <div>
+        <h1>숫자 : {number}</h1>
+        <button onClick={add}>더하기</button>
+    </div>
 </>
 );
 };
