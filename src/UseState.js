73f05f892501c54af1e2f324b8221c3777d8444b
chan jun <ckswns879@naver.com>
2023-05-09 17:54:02 +0900

    feat: UseState

--- src/UseState.js
+++ src/UseState.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import Sub from "./Sub";
 
 
 
 function UseState() {
+
 //let number = 1; // 이렇게 하면 상태값 아님
 const [number, setNumber] = useState(1);  //상태값 //React안에 hooks라이브러리
 
@@ -16,6 +18,7 @@ return (
     <div>
         <h1>숫자 : {number}</h1>
         <button onClick={add}>더하기</button>
+        <Sub />
     </div>
 </>
 );
