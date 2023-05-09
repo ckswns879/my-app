0acdb42a13d2f3904d6a394cdbb2ccc7ffba2e34
chan jun <ckswns879@naver.com>
2023-05-09 17:38:15 +0900

    feat: 자식컴포넌트추가

---
+++ src/Sub.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// rsc입력시 기본틀을 자동으로 만들어줌(확장자설치)
+const Sub = () => {
+
+// 함수제공
+
+    return (
+        <div>
+            <h1>Sub입니다.(자식컴포넌트)</h1>
+        </div>
+    );
+};
+
+export default Sub;
