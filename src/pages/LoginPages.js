6c9f0ff5cec560dfabc3eb5f759965ac31de34f9
chan jun <ckswns879@naver.com>
2023-05-31 17:42:37 +0900

    feat: component page

---
+++ src/pages/LoginPages.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header';
+import Login from '../components/login/Login';
+import Footer from '../components/Footer';
+
+
+const LoginPages = () => {
+    return (
+        <div>
+            <Header />
+            <Login />
+            <Footer />
+        </div>
+    );
+};
+
+export default LoginPages;
