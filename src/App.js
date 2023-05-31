b5fbe55572accee4692078435679ae10381da5bb
chan jun <ckswns879@naver.com>
2023-05-31 17:20:37 +0900

    refactor: refactoring

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import './App.css';
-
-
-// 실행과정 (index.html) - spa single page application
-// jsx문법 html과 유샤한구조를 갖고 있어 사용하기 간편하다
-
-
-// react는 하나의DOm만 리턴함
-// 변수선언은 let 혹은 const로만 해야함        let은 변수 const는 상수
-// if 사용 불가능 -> 삼항연산자(조건 ? 값(true) : 값(false))
-// 조건부렌더링 (조건 && 값(true))   *false일땐 안나옴
-// css디자인 
-// 1. 내부에는방법
-// 2. 외부 파일에 적는방법
-// 3. 라이브러리사용 (부트스트랩, componet-styled)
-
-
-let a = 10; //변수
-const b = 20; //상수
+import LoginPages from './pages/LoginPages';
+// import UseRef from './UseRef';
+// import UseMemo from './UseMemo';
+// import UseEffect from './UseEffect';
+// import UseState from './UseState';
+// import UseStateTest from './UseStateTest';
+// import Apptest from './Apptest';
+// import ComponentsStyle from './Components_Style';
 
 
 function App() {
-  
-  let c; 
-  console.log(1,c); //변수는있는데 값이없어서 undefined라고출력됨 *java = null 과 비슷
-
-  const mystyle = {color:'red'};
   return (
     
-<div>
-<div style={mystyle}>if 사용 불가능 / 삼항연산자(조건 ? 값(true) : 값(false)) ex) {a === 10 ? '10입니다' : '10이아님니다'}</div> {/* css디자인 1.내부에는방법 */}
-<h1 className='css2'>조건부렌더링 ex) {b === 20 && '20입니다.'}, {b === 10 && '20입니다.'}   {/*false라 출력안됨*/}</h1>
-</div>
+    <div>
+    {/* <Apptest /> */}
+    {/* <UseState /> */}
+    {/* <UseStateTest /> */}
+    {/* <UseEffect /> */}
+    {/* <UseMemo /> */}
+    {/* <UseRef /> */}
+    {/* <ComponentsStyle /> */}
+    <LoginPages />
+    </div>
 
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import UseRef from './UseRef';
-//import UseMemo from './UseMemo';
-//import UseEffect from './UseEffect';
-//import UseState from './UseState';
-//import UseStateTest from './UseStateTest';
-//import Apptest from './Apptest';
-import ComponentsStyle from './Components_Style';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <Apptest /> */}
-    {/* <UseState /> */}
-    {/* <UseStateTest /> */}
-    {/* <UseEffect /> */}
-    {/* <UseMemo /> */}
-    {/* <UseRef /> */}
-    <ComponentsStyle />
+    <App />
   </React.StrictMode>
 );
 
