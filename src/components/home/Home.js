99819adcba1c69e92ad921d0a09d7f0ec573f427
chan jun <ckswns879@naver.com>
2023-06-01 16:59:01 +0900

    feat:  props(변수,함수 passing하기)

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,10 +7,11 @@ const Home = (props) => {
 
     
     // 구조분할 할당
-    const {boards, setBoards} = props;
+    const {boards, setBoards, number, setNumber} = props;
     return (
         <div>
-            <h1>홈페이지입니다</h1>
+            <h1>홈 : {number}</h1>
+            <button onClick={(e) => setNumber(number +1)}>번호증가</button>
             <button onClick={() => setBoards([])}>전체삭제</button>
             {boards.map((board) => <h3>제목 : {board.title} 내용 : {board.contant}</h3>)}
         </div>
