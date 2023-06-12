8c716e75a52e75082e931e358f7d5f04145985cc
chan jun <ckswns879@naver.com>
2023-06-12 12:47:49 +0900

    feat: styled-component 스타일 상속, 스타일확장

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,6 +5,10 @@ const StyledDeleteButton = styled.button`
 background-color: ${(props) => (props.user.username === 'ssar'?'blue':'red')};
 color: white;
 ;`
+const StyledAddButton = styled(StyledDeleteButton)`  //스타일확장(위에 스타일을 받아오기 때문에 순서중요)
+background-color: ${(props) => (props.user.username === 'ssar'?'yellow':'red')};
+color: black;
+;`
 
 // Function 방식
 // class 방식
@@ -17,7 +21,7 @@ const Home = (props) => {
     return (
         <div>
             <h1>홈 : {number}</h1>
-            <button onClick={(e) => setNumber(number +1)}>번호증가</button>
+            <StyledAddButton user={user} onClick={(e) => setNumber(number +1)}>번호증가</StyledAddButton>
             <StyledDeleteButton user={user} onClick={() => setBoards([])}>전체삭제</StyledDeleteButton>
             {boards.map((board) => <h3>제목 : {board.title} 내용 : {board.contant}</h3>)}
         </div>
